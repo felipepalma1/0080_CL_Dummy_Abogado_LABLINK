93ddbc0bf3941acd2474ced28704c3d592cdcece
Felipe Palma <felipepalmacastro@gmail.com>
2020-10-02 03:25:21 -0300

    Agregado html:head con informacion d estilo

---
+++ dummy_abogado.php
@@ -0,0 +1,61 @@
+<html>
+    <head>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Laboralink.com</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Red+Hat+Display:400,400i,500,500i,700,700i,900,900i&amp;display=swap" rel="stylesheet"> 
+        <link href="../admin.css" rel="stylesheet" type="text/css">
+        <link href="admin.css" rel="stylesheet" type="text/css">
+        <script type="text/javascript" src="/ckeditor/ckeditor.js"></script>
+
+        <link href="/registro/css/bootstrap.css" rel="stylesheet">
+        <link href="/registro/css/autofaro.css" rel="stylesheet">
+        <link href="/registro/css/all.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Red+Hat+Display:400,400i,500,500i,700,700i,900,900i&amp;display=swap" rel="stylesheet"> 
+        <script src="/registro/js/jquery.min.js"></script>
+
+        <script type="text/javascript">
+        function actualizar(){
+            window.location.reload(true);
+        }
+        </script>
+        <script type="text/javascript" src="/registro/js/sound-onclick.js"></script>
+
+    </head>
+    <body>
+    <div class="tabla_admin">
+     <div class="titulomenu">Pedro Simon</div>
+     	<form action="products.php" method="get">
+
+        <input type="hidden" name="desc_typep" value="">
+        <input type="hidden" name="hi" value="">
+        <input type="hidden" name="id_typep" value="1">
+
+      <table width="100%" border="0" align="center" cellpadding="2" cellspacing="0">
+      <tbody><tr class="catalogo">
+   			  <td align="center" class="topcat">17.118.356-1  </td>
+   			  <td class="topcat">
+            Materia: Despido         	</td>
+      </tr>
+    </tbody></table>
+      <table width="100%" border="0" align="center" cellpadding="2" cellspacing="0">
+      <tbody><tr class="catalogo">
+  			<td align="right" class="">
+  		  	      <input type="SUBMIT" name="list" value="Ver Antecedentes" class="btn btn_primario pl-5 pr-5 mb-3">
+  			</td> 
+        <td align="right" class="">
+          <a href="products.php?c=1&amp;creaproc=1" class="btn btn_primario pl-5 pr-5 mb-3">Expediente Virtual</a>
+        </td> 
+        <td align="right" class="">
+          <a href="products.php?c=1&amp;creaproc=2" class="btn btn_primario pl-5 pr-5 mb-3">Comunicados</a>
+        </td> 
+        <td align="right" class="">
+          <a href="#" class="btn btn_primario pl-5 pr-5 mb-3">Calendario</a>
+        </td>
+  		  </tr>
+      </tbody></table> 
+      </form>
+    </div>
+    </body>
+</html>
